91d69c0b9b59d16fa6b6ff218a775695bb50ffc6
zxspectrr <zx@example.com>
2024-12-07 18:49:30 +0000

    wip

--- src/advent/day6.clj
+++ src/advent/day6.clj
@@ -2,5 +2,38 @@
   (:require [advent.utils :as u]
             [clojure.string :as str]))
 
-(def lines
-  (u/read-lines "resources/day6/small.txt"))
+
+(def state
+  {:grid
+   (->> (u/read-lines "resources/day6/small.txt")
+        (map-indexed
+          (fn [idx-y line]
+            (map-indexed
+              (fn [idx-x char] {:char char :x idx-x :y idx-y}) line)))
+        (flatten))
+   :direction :up})
+
+(defn find-starting-point []
+  (-> (filter #(= (:char %) \^) (:grid state)) (first)))
+
+(defn start []
+  (let [guard (find-starting-point)
+        grid (:grid state)
+        filtered-grid (filter #(not= (:char %) \^) grid)
+        adjusted-grid (conj filtered-grid (assoc guard :char \X))]
+    (assoc state :grid adjusted-grid)))
+
+(comment
+  (assoc (find-starting-point) :char \#)
+
+  (conj)
+
+  (count (:grid state))
+  (count (filter #(not= (:char %) \^) (:grid state)))
+  (start)
+
+  (count (filter #(= (:char %) \X) (:grid (start))))
+
+  ,)
+
+
