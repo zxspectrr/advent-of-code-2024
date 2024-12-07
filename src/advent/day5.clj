23ac9aaba4785a5ad6a159d45baa6b0e62efcfea
zxspectrr <zx@example.com>
2024-12-07 17:22:31 +0000

    Day 5 part 1 complete

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -1,8 +1,9 @@
 (ns advent.day5
   (:require [advent.utils :as u]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
-(def input "resources/day5/small.txt")
+(def input "resources/day5/input.txt")
 
 (def rules
   (->> (slurp input)
@@ -11,12 +12,32 @@
        (str/split-lines)
        (map #(->> (u/split-and-trim % #"\|") (mapv u/parse-int)))))
 
-(def pages
+(def page-sets
   (->> (slurp input)
        (#(str/split % #"\s\n"))
        (second)
        (str/split-lines)
        (map #(->> (u/split-and-trim % #",") (mapv u/parse-int)))))
+
+(defn satisfies-rule [pages rule]
+  (let [filtered (filter (set rule) pages)]
+    (if (< (count filtered) 2)
+      true
+      (= (first filtered) (first rule)))))
+
+(defn valid-page-set [page-set]
+  (every? #(satisfies-rule page-set %) rules))
+
+(defn middle-page [pages]
+  (->> (/ (count pages) 2)
+       (Math/floor)
+       (int)
+       (nth pages)))
+
+(defn part1 []
+  (->> (filter valid-page-set page-sets)
+       (map middle-page)
+       (reduce +)))
 
 (comment)
 
