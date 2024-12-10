5bc20e2ccd20663f045b458c58c0bc7f689d7f8c
zxspectrr <zx@example.com>
2024-12-10 13:02:01 +0000

    wip

--- src/advent/day7.clj
+++ src/advent/day7.clj
@@ -4,17 +4,35 @@
 (defn load-data []
    (->> (u/read-lines "resources/day7/small.txt")))
 
+(defn do-numbers [a b remaining]
+  (if (nil? b)
+    a
+    (map (fn [op]
+            (let [result (op a b)]
+              (if (empty? remaining)
+                result
+                (do-numbers result (first remaining) (rest remaining)))))
+         [+ - * /])))
+
 (defn try-numbers [numbers]
   (let [a (first numbers)
         b (second numbers)
-        others (rest (rest numbers))]
-    (map (fn [x] (x a b)) [+ - * /])))
+        remaining (rest (rest numbers))]
+    (->> (do-numbers a b remaining)
+         (flatten))))
 
+(defn check-numbers [desired-result numbers]
+  (->> (try-numbers numbers)
+       (filter (partial = desired-result))
+       (count)))
 
 (comment
-  (try-numbers [17 5])
+  (first (rest [1]))
+  (first [])
+  (check-numbers [81 40 27] 3267)
 
-  (map (fn [x] (x 10 19)) [+ - * /])
+  (check-numbers 190 [10 19])
 
-  ,)
+
+,)
 
