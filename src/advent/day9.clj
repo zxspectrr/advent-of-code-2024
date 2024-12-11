2be67102e51af453b88bfc556caf0adaa9607147
zxspectrr <zx@example.com>
2024-12-11 14:50:55 +0000

    Day 9 start

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -1,5 +1,24 @@
 (ns advent.day9
   (:require [advent.utils :as u]))
+  ;(:require [advent.utils :as u]))
 
 (def diskmap
-  (->> (slurp "resources/day9/small.txt")))
+  (->> (slurp "resources/day9/small.txt")
+       (partition-all 3 3)))
+       ;(reduce (fn [acc x]
+       ;          {})
+       ;        {:i 0})))
+
+
+(comment
+  (->> (partition-all 2 2 "12345")
+       (map-indexed (fn [idx [file free]]
+                      {:id idx
+                       :file (u/parse-int (str file))
+                       :free (if free (u/parse-int (str free)) 0)})))
+  ;(map ""))
+
+
+
+
+  "")
