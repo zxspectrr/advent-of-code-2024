9293a5e1e882db0f6253f1908d20d99289482cd0
zxspectrr <zx@example.com>
2024-12-11 15:41:41 +0000

    wip

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -1,5 +1,6 @@
 (ns advent.day9
-  (:require [advent.utils :as u]))
+  (:require [advent.utils :as u]
+            [clojure.string :as str]))
 
 (def diskmap
   (slurp "resources/day9/small.txt"))
@@ -16,11 +17,47 @@
                  filestr (apply str (repeat file-int idx))
                  freestr (apply str (repeat free-int \.))]
              (apply str [filestr freestr]))))
-       (apply str)))
+       (apply str)
+       (vec)))
+
+(defn shift-block [blocks]
+  (let [blocks-str (apply str blocks)
+        number (last (filter #{\1 \2 \3 \4 \5 \6 \7 \8 \9} blocks))
+        idxa (str/last-index-of blocks-str number)
+        idxb (str/index-of blocks-str \.)]
+    (-> (assoc blocks idxb number)
+        (assoc idxa \.))))
 
 
 (comment
+  (def blocks expanded)
 
+  (->> (shift-block blocks)
+       (apply str))
+
+
+
+
+  (str/last-index-of (apply str expanded) \9)
+  (str/index-of (apply str expanded) \.)
+
+  (.indexOf expanded \0)
+
+  (last (filter #{\1 \2 \3 \4 \5 \6 \7 \8 \9} expanded))
+  (first (filter #{\.} expanded))
+
+  (last (re-seq #"\d" (str expanded)))
+
+  (->> (re-matcher #"\d" (str expanded))
+       (re-seq))
+
+
+
+
+
+  (def expanded (expand-diskmap diskmap))
+
+  (vec "test")
   (expand-diskmap diskmap)
 
   (repeat 2 "test")
