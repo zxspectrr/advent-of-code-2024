d591c55f91fbcf229c9adc7229fc5f842cdd642e
zxspectrr <zx@example.com>
2024-12-20 16:09:53 +0000

    wip

--- src/advent/day10.clj
+++ src/advent/day10.clj
@@ -12,7 +12,39 @@
                     grid)
        (flatten)))
 
+(defn find-item [[y x]]
+  {:y y :x x :height (get-in grid [y x])})
+
+(defn find-starting-points []
+  (filter #(= 0 (:height %)) grid-list))
+
+(defn outside-grid? [y x]
+  (let [max-x (-> (first grid) (count) (dec))
+        max-y (-> (count grid) (dec))]
+    (or (< y 0) (< x 0)
+        (> y max-y) (> x max-x))))
+
+
+(defn find-neighbours [point]
+  (let [{:keys [height x y]} point
+        neighbour-coords [[(inc y) x] [(dec y) x]
+                          [y (inc x)] [y (dec x)]]
+        neigbours (map find-item neighbour-coords)]
+    neigbours))
+
+(defn walk [point]
+  (let [{:keys [height x y]} point]))
+
+(defn find-end-points [starting-point])
 
 (comment
+
+  (find-neighbours {:height 1 :x 3 :y 3})
+  (find-item [1 2])
+  (outside-grid? 0 -1)
+
+  (find-item [0 3])
+
+
   (u/parse-int (str \2))
   "")
